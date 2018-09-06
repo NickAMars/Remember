77caca366396d1de1d55e6f26f5d1a3f7b456fcf
NicksComingUp <niclaus.a@gmail.com>
2018-09-06 17:58:32 -0500

    added typescript for delete method

--- client/src/components/Remember/Remember.js
+++ client/src/components/Remember/Remember.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import {deleteCard} from '../../actions';
+import {deleteCard,  updateForm} from '../../actions';
 import {Card} from './Card';
 import Form from './Form';
 import PropTypes from 'prop-types';
-/*
-Need to Give every card a unique id
-
-*/
 export class Remember extends Component{
   render(){
-    // console.log(this.props);
+    console.log(this.props);
     return (
       <div>
         <h1 className="heading-primary">Remember</h1>
@@ -20,17 +16,18 @@ export class Remember extends Component{
         {
          /*creating card components*/
          this.props.cards.map((card, index) => {
-           return (<Card  key={card.id}  {...card} deleteCard={this.props.deleteCard} />);
+           return (<Card  key={card.id}  {...card} deleteCard={this.props.deleteCard} updateForm={this.props.updateForm} />);
          })
         }
-       </div>
-    </div>
-  );
+        </div>
+      </div>
+    );
   }
 }
 
 Remember.propTypes ={
-  cards: PropTypes.array.isRequired
+    cards: PropTypes.array.isRequired,
+    deleteCard: PropTypes.func
 }
 
 export const mapStateToProps = (state) => {
@@ -39,5 +36,5 @@ export const mapStateToProps = (state) => {
   };
 }
 
-export default connect(mapStateToProps,{deleteCard})(Remember);
+export default connect(mapStateToProps,{deleteCard, updateForm})(Remember);
 // export default connect(null, {deleteCard})(Card);
