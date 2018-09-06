9219c6c24705337197e4877a570ad12eae4033a0
NicksComingUp <niclaus.a@gmail.com>
2018-09-06 18:04:46 -0500

    takes in boolean and card object

---
+++ client/src/reducer/updateForm.js
@@ -0,0 +1,15 @@
+import { UPDATE_FORM } from '../actions';
+
+
+// send a true value to the form field which  display icon button to update card
+// @params boolean , card
+export default  function (state = {}, action) {
+  switch(action.type){
+    case UPDATE_FORM:
+    // @params boolean , card
+    return action.payload;
+      //return [...state, action.payload];
+    default:
+      return state;
+  }
+}
