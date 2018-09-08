18f526f6f6e9de06505f9f3393a395719b12b980
NicksComingUp <niclaus.a@gmail.com>
2018-09-08 18:39:24 -0500

    plan for how im going to work with the signin page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 // import {Description} from './Description';
 // import {SignIn} from './SignIn';
 /*
-Thinking that if i created the design before hand
- was a mistake.
- Rather create the design  while im working on the components
+Main tags going to rap these and take up the remaining view port height of the front page
+  Description going to be to the side take up 70% of the page
+  SignIn going to have 30% of the page
+  SignIn going to have google Icon button  and a facebook Icon button
+  that you can use to sign in aswell.
+
 */
 
 
@@ -18,7 +21,10 @@ export const Landing = () =>{
             <span className="heading-primary">Future Memory</span>
           </div>
         </header>
-        <Link to="/remember">Remember &rarr;</Link>
+        <Link to="/mastercard/subcards">Remember &rarr;</Link>
       </div>
     );
 }
+
+
+// might put a forget password field in this application
