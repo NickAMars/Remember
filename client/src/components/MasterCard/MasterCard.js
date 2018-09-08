6e8979b0e69cd4c5efa53f3508aa67c68494e0c7
NicksComingUp <niclaus.a@gmail.com>
2018-09-08 18:40:29 -0500

    plan for the master card creation

---
+++ client/src/components/MasterCard/MasterCard.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+export class MasterCard extends Component{
+  render(){
+    return (<div> MasterCard </div>);
+  }
+}
+
+/*
+  What do i want this field to have?
+  Step 1: Small card element with title size around 80-100px height and 300px width
+  Step 2: Title center ()
+  Step 3: want an image a uploaded image to be under the text.
+  Step 4: I want a overlay over the image to make it not interfair with the text.
+          probably dark with opacity:.3.
+
+  There will be a small navigation layout where users can click between 'title', 'image'
+  Title is not require if user uploads image.
+  Image is not require if user uploads a title.
+
+  Restriction- The title field should only be two row.
+
+
+  General overview:
+  Where is this card going to go in the end?
+  Objectively its going to be stored with other mastercards in a section called Memory
+  and also in a section which shows how long user spend on the card called Statics
+*/
