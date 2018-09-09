eb2df5f206dde4824a9cae52f3103351819272fa
NicksComingUp <niclaus.a@gmail.com>
2018-09-09 18:10:51 -0500

    added subcard schema

---
+++ models/SubCards.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema} = mongoose;
+
+const subCardSchema = new Schema({
+  title: String,
+  descriptions: String
+});
+
+module.exports = mongoose.model('subcards', subCardSchema);
