f46132d103ff64312f278c29238fbe2bb4b62f18
NicksComingUp <niclaus.a@gmail.com>
2018-09-10 23:09:56 -0500

    adding routes and testing them with post man

--- controllers/masterCard.js
+++ controllers/masterCard.js
@@ -1,3 +1,22 @@
 const masterCards = require('../models/MasterCards');
 const passport        = require('passport');
-module.exports = {};
+module.exports = {
+  masterCards : (req,res) =>{
+    console.log("show all master cards ");
+    res.send("All master cards");
+  },
+  createMasterCards : (req,res) =>{
+    console.log("create one master card");
+    res.send("create one master card");
+  },
+  updateMasterCards : (req,res) =>{
+    console.log("update a master card");
+    res.send("update a master card");
+  },
+  deleteMasterCards : (req,res) =>{
+    console.log("delete a master card");
+    res.send("delete a master card");
+  }
+};
+
+// CRUD
--- controllers/subCard.js
+++ controllers/subCard.js
@@ -1,3 +1,30 @@
 const subCards = require('../models/SubCards');
 const passport        = require('passport');
-module.exports = {};
+module.exports = {
+
+subCards: (req,res) =>{
+  console.log("All sub cards");
+  res.send("All sub cards");
+},
+subCardForm: (req,res) =>{
+  console.log("Form for new subCard");
+  res.send("Form for new subCard");
+},
+addsubCard: (req,res) =>{
+  console.log("adding new subcard");
+  res.send("adding new subcard");
+},
+changeSubCard: (req,res) =>{
+  console.log("changing existing subcard");
+  res.send("changing exist subcard");
+},
+deleteSubCard: (req,res) =>{
+  console.log("delete existing subcard");
+  res.send("delete existing subcard");
+},
+storeMultipleSubCards: (req,res)=>{
+  console.log("add multiple subcards");
+  res.send("add multiple subcards");
+}
+
+};
