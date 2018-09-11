4828fa99c1fb39df247f36bd092baa338c28a976
NicksComingUp <niclaus.a@gmail.com>
2018-09-10 23:06:57 -0500

    only authentication stuff

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,14 +2,19 @@
 const User = require('../models/User');
 const passport        = require('passport');
 module.exports = {
-  signup:       passport.authenticate('local-login', {
-    successRedirect : '/profile',
-    failureRedirect : '/create',
-  }),
+
+  loginForm: (req,res)=>{
+    console.log('login page');
+    res.send('login form page for users');
+  },
   login: (req,res)=>{
     console.log(req.user);
-    res.redirect('/profile');
+    res.redirect('/api/profile');
   },
+  signup:       passport.authenticate('local-login', {
+    successRedirect : '/api/profile',
+    failureRedirect : '/api/login',
+  }),
   google:       passport.authenticate('google', {scope : ['email']}),
   googleCB:     (req, res) =>{res.redirect('/profile')},
   facebook:     passport.authenticate('facebook', {scope : ['email']}),
