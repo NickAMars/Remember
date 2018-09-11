ad07a9e0502299fb6586e3dd9786e426b7b4a9d0
NicksComingUp <niclaus.a@gmail.com>
2018-09-10 23:13:19 -0500

    users route

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,17 @@
 const Controller      = require(`../controllers`);
-const authController      = require(`../controllers/authController`);
-const passport        = require('passport');
 
 
 module.exports = app => {
-  // app.post('/test', Controller.test);
-  app.get('/profile',Controller.profile);
-
-  // File for out or signing up existing user
-  app.post('/auth/signup',authController.signup);
-  app.post('/auth/login',passport.authenticate('local-login'),authController.login);
-  // app.post('/auth/google/callback',Controller.local);
-  app.get('/auth/google',authController.google);
-  app.get('/auth/google/callback',passport.authenticate('google'),authController.googleCB);
-  app.get('/auth/facebook',authController.facebook);
-  app.get('/auth/facebook/callback',passport.authenticate('facebook'), authController.facebookCB);
-  app.get('/api/logout', authController.logout);
+  // get my profile
+  app.get('/api/profile',Controller.profile); // shows your profile page
+  // change my profile
+  app.put('/api/profile/:id',Controller.update );
+  // delete my profile
+  app.delete('/api/profile/:id',Controller.delete );
 }
+
+
+/*
+ authentication already takes care of this
+ app.post('/api/profile',Controller.create );
+*/
