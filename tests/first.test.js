52eb1a68ce425a3b3284f433b675055de643c2f0
NicksComingUp <niclaus.a@gmail.com>
2018-09-11 22:49:35 -0500

    remove a file

--- tests/first.test.js
+++
@@ -1,7 +0,0 @@
-
-
-
-describe('actions', ()=>{
-       test('1 =1 ', () => expect("1").toEqual("1"));
-  // test('create action to add card',()=>expect(actions.addCard(card)).toEqual(ADDCARD));
-})
