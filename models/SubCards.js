1958b78316fb5275536809ae7540bccd8ec537a1
NicksComingUp <remember.marsman@gmail.com>
2018-09-15 21:35:17 -0500

    added the user info to sub card

--- models/SubCards.js
+++ models/SubCards.js
@@ -3,7 +3,11 @@ const { Schema} = mongoose;
 
 const subCardSchema = new Schema({
   title: String,
-  descriptions: String
+  descriptions: String,
+  user:{
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "users"
+  }
 });
 
 module.exports = mongoose.model('subcards', subCardSchema);
