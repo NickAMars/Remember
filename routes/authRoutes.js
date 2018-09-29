e1a3298198dee62dd07ad5dd6ee1c48531bb5f30
NicksComingUp <remember.marsman@gmail.com>
2018-09-28 23:25:31 -0500

    taking out the get from from the server

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,10 +6,10 @@ module.exports = app => {
 
   // File for out or signing up existing user
   app.post('/auth/signup',authController.signup);
-  // inital form page of login 
-  app.get('/auth/login', authController.loginForm);
+
   //click sign up button check information send back
   app.post('/auth/login',passport.authenticate('local-login'),authController.login);
+
   // app.post('/auth/google/callback',Controller.local);
   app.get('/auth/google',authController.google);
   app.get('/auth/google/callback',passport.authenticate('google'),authController.googleCB);
