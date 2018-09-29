4857383ca552d4bca4e2b2e6471499c49bdb982f
NicksComingUp <remember.marsman@gmail.com>
2018-09-28 23:28:13 -0500

    added redux thunk middle ware to the parent component

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import React from  'react';
 import ReactDOM from 'react-dom';
 import './style/css/main.css';
 import App from './components/App';
+// placing this at the top level
+import axios from 'axios';
 // creating the redux store and listener
-import reducer from './reducer';
-import { createStore } from 'redux';
+import reducers from './reducer';
+import { createStore, applyMiddleware  } from 'redux';
 import { Provider } from  'react-redux';
-const store = createStore(reducer);
+import thunk from 'redux-thunk';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 /*
   Going to use redux-thunk for the middleware
