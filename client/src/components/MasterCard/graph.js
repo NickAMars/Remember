21994f62321a026ea8093439232f7e87ab3f324f
NicksComingUp <remember.marsman@gmail.com>
2018-10-07 21:08:54 -0500

    this is where im going to put the d3 graph

---
+++ client/src/components/MasterCard/graph.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+
+export const  Graph  = (props)=>{
+
+return (
+      <table  className="table">
+    {/*     <thead>
+                <tr className="table__hr">
+                    <th className="table__th" colSpan="6">D3 GRAPH</th>
+                </tr>
+          </thead>
+          <tr className= "table__tr">
+            <th className="table__hr--sub">&nbsp;</th>
+            <th className="table__hr--sub">CARD 1</th>
+            <th className="table__hr--sub">CARD 2</th>
+            <th className="table__hr--sub">CARD 3</th>
+            <th className="table__hr--sub">CARD 4</th>
+            <th className="table__hr--sub">CARD 5</th>
+          </tr>
+          <tr className= "table__tr">
+            <td className="table__tc--1">Monday</td>
+            <td className="table__tc--1">10</td>
+            <td className="table__tc--1">20</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+
+          </tr>
+          <tr className= "table__tr">
+            <td className="table__tc--1">Tuesday</td>
+            <td className="table__tc--1">10</td>
+            <td className="table__tc--1">20</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+
+          </tr>
+          <tr className= "table__tr">
+            <td className="table__tc--1">Wednesday</td>
+            <td className="table__tc--1">10</td>
+            <td className="table__tc--1">20</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+
+          </tr>
+          <tr className= "table__tr">
+            <td className="table__tc--1">Thursday</td>
+            <td className="table__tc--1">10</td>
+            <td className="table__tc--1">20</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+          </tr>
+          <tr className= "table__tr">
+            <td className="table__tc--1">Friday</td>
+            <td className="table__tc--1">10</td>
+            <td className="table__tc--1">20</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+
+          </tr>
+          <tr className= "table__tr">
+            <td className="table__tc--1">Saturday</td>
+            <td className="table__tc--1">10</td>
+            <td className="table__tc--1">20</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+          </tr>
+          <tr className= "table__tr">
+            <td className="table__tc--1">Sunday</td>
+            <td className="table__tc--1">10</td>
+            <td className="table__tc--1">20</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+            <td className="table__tc--1">40</td>
+          </tr>
+      */} 
+      </table>
+    );
+}
