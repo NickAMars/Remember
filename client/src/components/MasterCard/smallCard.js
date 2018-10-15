82616ced396534f7ab7bb8b030b3ff6d2004a249
NicksComingUp <remember.marsman@gmail.com>
2018-10-14 23:12:50 -0500

    sending infomation to small cards

--- client/src/components/MasterCard/smallCard.js
+++ client/src/components/MasterCard/smallCard.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 
 // functional component that display one card item
 export  const SmallCard  = (props)=>{
+  const {title, date } = props.cards;
+  // console.log(title,date);
+  // console.log(props);
+
   return (
     <li className="list__items">
       <Link  to="/main" className="list__links">
-        <span className="list__title">Master Card</span>
-        <span className="list__date">time created</span>
+        <span className="list__title">{title}</span>
+        <span className="list__date">{date}</span>
       </Link>
     </li>
   );
