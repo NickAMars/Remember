f6a2aace276e116090e385a71bd5a7220f829877
NicksComingUp <remember.marsman@gmail.com>
2018-11-11 18:25:14 -0600

    create test for server

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,19 @@
+ import axios from 'axios';
 export const ADD_CARD= "ADD_CARD",
  UPDATE_CARD= "UPDATE_CARD",
  DELETE_CARD="DELETE_CARD",
  UPDATE_FORM="UPDATE_FORM",
  TEST_CARD = "TEST_CARD";
 
-export function addCard(card){
+
+ export const serverTest = (newCard)=>{
+  return async dispatch => {
+     const res = await axios.get('/api/profile/MasterCard');
+     dispatch( {type: TEST_CARD, payload: res.data } );
+   }
+ }
+
+export const addCard = (card)=>{
   // this process is never to go to the back end
   // i will use a cookie to store the cards so that when
   // the user refresh the screen all the cards arnt lost
@@ -14,7 +23,7 @@ export function addCard(card){
     payload: card
   };
 }
-export function deleteCard(id){
+export const deleteCard = (id)=>{
   return {
   type: DELETE_CARD,
   payload: id
@@ -23,7 +32,7 @@ export function deleteCard(id){
 /*
   @param ID, Title, Description
 */
-export function updateCard(newCard){
+export const updateCard = (newCard)=>{
   return {
   type: UPDATE_CARD,
   payload: newCard
@@ -34,22 +43,16 @@ export function updateCard(newCard){
  send a true value to the form field which  display icon button to update card
   @param ID, Title, Description
 */
-export function updateForm(newCard){
+export const updateForm = (newCard)=>{
   return {
   type: UPDATE_FORM,
   payload: newCard
   }
 }
 
-/* For onEnter method */
-// const dummy = [ {id:"kiss", title:"Toni", date:"november 2,2017"},
-//                 {id:"love", title:"Parent", date:"september 4, 2013"},
-//                 {id:"No", title:"Conflict", date:"september 16, 2018"},
-//                 {id:"Dreams", title:"Realize", date:"March 14, 2018"}
-//             ];
-// export function testCard(card){
-//   return {
-//     type: TEST_CARD,
-//     payload:  dummy
-//   };
-// }
+//Test dispatch method from server
+// export const testServer = () =>
+//   async dispatch => {
+//     const res = await axios.get('/api/profile/MasterCard');
+//     dispatch( {type: TEST_CARD, payload: res.data } );
+//
--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import  cards  from './cardOperations';
 import  formupdate  from './updateForm';
-// import  test  from './test_reducer';
+import  test  from './test_reducer';
 const rootReducer = combineReducers({
   cards,
-  formupdate
-  // ,test
+  formupdate,
+  test // to test server with actionCreater
 });
 
 export default rootReducer;
