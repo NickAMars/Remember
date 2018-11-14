89cecd74b6a54a2b94992ea348d56ad1975aea21
NicksComingUp <remember.marsman@gmail.com>
2018-11-13 19:37:55 -0600

    adding button component to the form

--- client/src/components/MultipleCards/Form.js
+++ client/src/components/MultipleCards/Form.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { addCard, updateForm, updateCard } from '../../actions';
 import  uuidv1 from 'uuid/v1'
 import PropTypes from 'prop-types';
+import {Buttons} from  './Buttons';
 /*
     I want the update method to use the same form.
     I want to make it obvious that the forms beening use for the update
@@ -29,29 +30,13 @@ componentWillReceiveProps(nextProps){
     this.setState({firstRender: false, title, description});
   }
 }
-  Add = () => {
-    if(validate(this.state))
-      this.props.addCard({id:uuidv1() ,...this.state});
-      this.emptyState();
-      //fixes a problem after you ad the card of an updated field
-      this.props.updateForm({});
-  }
-  Update = () =>{
-    let {id} = this.props.formupdate;
-    let {title, description} = this.state;
-    this.props.updateCard({id,title,description});
-    this.emptyState();
-    this.props.updateForm({});
-  }
-  emptyState = () => {
-    this.setState({  title: '',description: '',firstRender: true});
-  }
+
 
   render(){
     let {display} = this.props.formupdate;
     return (
            <div>
-                <div className="input__box">
+                 <div className="input__box">
                   <input
                     autoFocus
                     className="input__field"
@@ -67,11 +52,29 @@ componentWillReceiveProps(nextProps){
                     value={this.state.description}
                     >
                   </textarea>
-                  <button onClick={this.Update}  className={display?"btn btn__update btn__update-show":"btn btn__update btn__update-hide"}>U</button>
-                  <button onClick={this.Add}  className="btn btn__add">+</button>
+                  <Buttons  display={display} Update={this.Update} Add={this.Add} Submit= {this.props.Submit}/>
                 </div>
           </div>
         );
+  }
+
+  Add = () => {
+    if(validate(this.state))
+      this.props.addCard({id:uuidv1() ,...this.state});
+      this.emptyState();
+      //fixes a problem after you ad the card of an updated field
+      this.props.updateForm({});
+  }
+
+  Update = () =>{
+    let {id} = this.props.formupdate;
+    let {title, description} = this.state;
+    this.props.updateCard({id,title,description});
+    this.emptyState();
+    this.props.updateForm({});
+  }
+  emptyState = () => {
+    this.setState({  title: '',description: '',firstRender: true});
   }
 }
 Form.propTypes ={
@@ -83,7 +86,7 @@ export const mapStateToProps = (state) => {
     formupdate: state.formupdate
   };
 }
-export const validate = ({title,description }) => {
+export const validate = ({title ,description }) => {
     if( title !== '' && description !== '') return true;
     return false;
 }
@@ -93,3 +96,9 @@ export default connect(mapStateToProps, {addCard, updateForm, updateCard})(Form)
 
 // for lifecycle methods
 //https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
+
+
+/*
+Trying to decrease size of components
+<button onClick={this.Update}  className={display?"btn btn__update btn__update-show":"btn btn__update btn__update-hide"}>U</button>
+<button onClick={this.Add}  className="btn btn__add">+</button>*/
