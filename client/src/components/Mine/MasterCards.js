4e0177d7c37609f2fdad9223f8f5ab24ead7bf7c
NicksComingUp <remember.marsman@gmail.com>
2018-11-19 21:43:49 -0600

    updating the master component to send arguement to children

--- client/src/components/Mine/MasterCards.js
+++ client/src/components/Mine/MasterCards.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import {Buttons} from './Buttons';
 import {CbPublic} from './CbPublic';
+import { deleteMaster, updateMaster, publicMaster} from '../../actions';
 // CbPublic
 // camil case
 // pass keys to this items when mapping over object
-export const MasterCards = (props) =>{
+// export const MasterCards = (props) =>{
   // update the date and the title with the props
-  const {title, date,id} = props.card;
-  return (
-        <li className="master__items">
-          <Buttons />
-          {/*checkbox goes here*/}
-          <CbPublic />
-          <Link  to={`/smallcards/${id}`} className="master__links">
+  export class MasterCards extends Component{
+    render(){
+      const {title, date,id} = this.props.masterinfo;
+      return (
+            <li className="master__items">
+              <Buttons  delMaster={this.props.deleteMaster} masterid={id}/>
+              <CbPublic pubinfo={this.props.masterinfo} />
+              <Link  to={`/smallcards/${id}`} className="master__links">
 
-            <span className="master__title">{title}</span>
-            <span className="master__date">{date}</span>
-          </Link>
-        </li>
-  );
-}
+                <span className="master__title">{title}</span>
+                <span className="master__date">{date}</span>
+              </Link>
+            </li>
+      );
+    }
+
+  }
+  export default connect(null,{deleteMaster, updateMaster, publicMaster} )(MasterCards);
+
+// }
