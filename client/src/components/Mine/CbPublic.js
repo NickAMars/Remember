29b5ac5017953c4da227522545c230c1b8608b83
NicksComingUp <remember.marsman@gmail.com>
2018-11-19 21:42:49 -0600

    adding the public method to the checkbox

--- client/src/components/Mine/CbPublic.js
+++ client/src/components/Mine/CbPublic.js
@@ -1,9 +1,25 @@
 import React from  'react';
+
 export  const CbPublic  = (props)=>{
-  // When they check this box their cards are to be shown public
-  // so i can have an action creater that get the users subcards and master cards and then send
-  // it to a public access pool which puts there name on the card as well
+  let checkbox = false;
+  const publicCheckBox = () =>{
+    if(!checkbox){
+      // call action creater sent from props here
+      console.log(props.pubinfo);
+      checkbox = true;
+    }else{
+      checkbox = false;
+    }
+  }
   return (
-    <input type="checkbox" id="create" className="master__checkbox"/>
+    <input type="checkbox" id="create" onClick={publicCheckBox} className="master__checkbox"/>
   );
 };
+
+// console.log(props.pubinfo);
+// When they check this box their cards are to be shown public
+// so i can have an action creater that get the users subcards and master cards and then send
+// it to a public access pool which puts there name on the card as well
+
+
+// const Mak
