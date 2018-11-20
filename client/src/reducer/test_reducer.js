060ebc2dd93a0072e172eb270cf878c9109782e1
NicksComingUp <remember.marsman@gmail.com>
2018-11-19 21:44:55 -0600

    making sure that i can send the id to the redux store

--- client/src/reducer/test_reducer.js
+++ client/src/reducer/test_reducer.js
@@ -1,4 +1,4 @@
-import { TEST_CARD, SUBMIT_CARD}  from '../actions/types';
+import { TEST_CARD, SUBMIT_CARD, DELETE_MASTER}  from '../actions/types';
 //
 //
 // // why doesnt this update the store
@@ -6,6 +6,10 @@ import { TEST_CARD, SUBMIT_CARD}  from '../actions/types';
 // This method is use just to test my server
 export default  function (state = "", action) {
   switch(action.type){
+    case DELETE_MASTER:
+      console.log(action.payload);
+    // do a filter removing the one with the id from the list
+      return `Working ${action.payload}`;
     case TEST_CARD:
       return action.payload;
     case SUBMIT_CARD:
