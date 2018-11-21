335957363622b4ac7336b02547eeec19ca349818
NicksComingUp <remember.marsman@gmail.com>
2018-11-20 20:06:01 -0600

    getting key info from store

--- client/src/reducer/test_reducer.js
+++ client/src/reducer/test_reducer.js
@@ -1,19 +1,22 @@
-import { TEST_CARD, SUBMIT_CARD, DELETE_MASTER}  from '../actions/types';
+import { TEST_CARD, SUBMIT_CARD, DELETE_MASTER,MASTER_FORM}  from '../actions/types';
 //
 //
 // // why doesnt this update the store
 
 // This method is use just to test my server
-export default  function (state = "", action) {
+export default  function (state = {}, action) {
   switch(action.type){
+    case MASTER_FORM:
+      // console.log(action.payload);
+    return action.payload;
     case DELETE_MASTER:
       console.log(action.payload);
     // do a filter removing the one with the id from the list
-      return `Working ${action.payload}`;
+      return {id:`Working ${action.payload}`};
     case TEST_CARD:
-      return action.payload;
+      return {id:action.payload};
     case SUBMIT_CARD:
-      return action.payload;
+      return {id:action.payload};
     default:
       return state;
   }
