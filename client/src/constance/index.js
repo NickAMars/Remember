1883881214a0d6cd30ddc8eca26e09794cf3cbdb
NicksComingUp <remember.marsman@gmail.com>
2019-01-04 23:05:12 -0600

    using the formate of the date which will be comming from the server

--- client/src/constance/index.js
+++ client/src/constance/index.js
@@ -3,3 +3,81 @@ export const CHECKBOX = "checkbox",
              FILE     = "file",
              SELECT   = "select",
              PASSWORD = "password";
+
+
+//Default data point for testing D3
+// export const data = [[0,80],
+//                       [100,100],
+//                       [200,30],
+//                       [300,50],
+//                       [400,40],
+//                       [500,80]];
+
+  const seven_days = 7*24*1000*60*60;
+
+
+
+// enum WEEK {
+//   SUN,
+//   MON,
+//   TUES,
+//   WED,
+//   THUR,
+//   FRID,
+//   SAT
+// }
+
+// return the date seven days back so i can get the time
+export const beginDate = (current_date) =>{
+  const year = current_date.getFullYear();
+  const month = current_date.getMonth();
+  const date = current_date.getDate();
+  const current = new Date(year, month, date);
+  const milliseconds = new Date(year, month, date) - seven_days;
+  // console.log(new Date(milliseconds));
+  // console.log(new Date(milliseconds));
+  // console.log([new Date(milliseconds), current ]);
+  return [new Date(milliseconds), current ];
+}
+
+// Sun = 0 Sat = 6
+// const WEEKDAY = 7;
+ // [0,1,2,3,4,5,6]
+// export const weekdays = (week_day) =>{ // returns an array
+//   const order_week = [];
+//   let i = week_day;
+//   while(order_week.length < WEEKDAY){
+//     order_week.unshift(i);
+//     if(i < WEEKDAY-1 ){ // 6
+//         i++;
+//     }else{
+//         i = 0;
+//     }
+//   }
+//
+//   // console.log(order_week);
+//   // console.log()
+// return order_week;
+// }
+
+ // {
+
+  // date
+  // const year = current.getFullYear();
+  // const month = current.getMonth();
+  // const date = current.getDate();
+
+  // const past_seven = new Date(current - seven_days);
+  // return new Date(current - seven_days);
+// }
+
+
+
+export const data = [
+  {date : new Date(2018, 11,28), time: 0 },
+   {date : new Date(2018, 11,29), time: 6000 },
+  {date : new Date(2018, 11, 30), time: 10000 },
+  {date : new Date(2019, 0,1), time: 9000 },
+  {date : new Date(2019, 0, 2), time: 7000 },
+{date :  new Date(2019, 0, 3), time: 3000 },
+{date : new Date(2019, 0,4), time: 100 } ]
