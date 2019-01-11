6515cd2f3cb60d289899e6fa1994dfc317cc8869
NicksComingUp <remember.marsman@gmail.com>
2019-01-11 11:30:34 -0600

    change the MasterCard component to Main

---
+++ client/src/components/Main/chart.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react'
+import {beginDate} from  '../../constance'
+// data,
+import * as d3 from "d3"
+
+const width = 500;
+const height = 500;
+const margin = { top: 15, right: 15,
+                  bottom: 20,left: 30};
+
+export class Chart extends Component{
+    state = {
+      line: null,
+      xScale: d3.scaleTime().domain(beginDate(new Date())).range([margin.left, width - margin.right]),
+      yScale: d3.scaleLinear().range([height - margin.bottom, margin.top ]),
+      areaGenerator:d3.area().curve(d3.curveLinear)
+    };
+          // areaGenerator: d3.line()
+    // .curve(d3.curveLinear)
+    // curve(d3.curveCardinal)
+
+
+    xAxis = d3.axisBottom().scale(this.state.xScale)
+            .ticks(7)
+            // .orient("bottom") doesnt work
+          .tickSize(0)
+          .tickFormat(d3.timeFormat("%m-%d"));
+    yAxis = d3.axisLeft().scale(this.state.yScale)
+          // .tickSize( 2)
+          .tickFormat(d3.format(".0s"));
+  componentDidMount(){
+    // get information needed from the server to do this
+  }
+  static getDerivedStateFromProps(nextProps, prevState){
+     // if (!nextProps.data) return null;
+     const {data} = nextProps;
+     const {xScale, yScale,  areaGenerator} = prevState;
+      const maxTime = d3.max(data, d => d.time)/(1000); // in second
+      yScale.domain([0, maxTime]);
+      // console.log(yScale(data[0].time));
+     areaGenerator.x(d => xScale(d.date));
+     areaGenerator.y0(height- margin.top);
+     areaGenerator.y1(d => yScale( d.time/1000) );
+     // areaGenerator.define(d => d.date !== null);
+     const line = areaGenerator(data);
+     // console.log(line);
+     return {line};
+
+  }
+    // manipulating the DOM (seems to only go when page load)
+  componentDidUpdate() {
+  d3.select(this.refs.xAxis).call(this.xAxis);
+  d3.select(this.refs.yAxis).call(this.yAxis);
+  // TITLE
+    d3.select(this.refs.svg)
+      .append("text")
+      .attr("x", width/2)
+      .attr("y", margin.top)
+      .attr("font-size", "1rem")
+      .attr("font-weight","900")
+      .attr("letter-spacing","10px")
+      .style("text-anchor", "middle")
+      .text("Top Five");
+    //DATE
+    d3.select(this.refs.svg)
+      .append("text")
+      .attr("x", width/2)
+      .attr("y",height)
+      .attr("font-size", "1rem")
+      .attr("font-weight","900")
+      .attr("letter-spacing","10px")
+      .style("text-anchor", "middle")
+      .text("DATE (mm/dd)");
+      //TIME
+    d3.select(this.refs.svg)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("font-size", "1.4rem")
+      .attr("x", -height/2)
+      .attr("y", margin.left -5)
+      .attr("dy", "-1.1em")
+      .attr("letter-spacing","10px")
+      .style("text-anchor", "middle")
+      .text("TIME (sec)");
+
+}
+
+  render(){
+    return(
+      <div className="topcards__graph">
+        <svg
+          ref='svg'
+          width={width}
+          height={height}>
+          <path d= {this.state.line} fill= 'orangered' strokeWidth="0" />
+          <g>
+            <g ref='xAxis' transform={`translate(0, ${height - margin.top})`} />
+            <g ref='yAxis' transform={`translate(${margin.left}, 0)`} />
+          </g>
+        </svg>
+      </div>
+    )
+  }
+}
+// stroke={'#EEE'}
+/* problem *The area goes under the graph so i apply a temorary fize */
---
+++ client/src/components/Main/index.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+// import {Header} from '../Header';
+// import { Link } from 'react-router-dom';
+import {TopFiveCard} from '../Helper/Cards';
+import {CreateMaster} from '../Helper/Form';
+// import {Graph} from  './graph';
+import {Chart} from  './chart';
+import { connect } from 'react-redux';
+// import {data} from  '../../constance'
+// import * as d3 from "d3"
+
+import {data} from  '../../constance'
+class MasterCard extends Component{
+  componentDidMount(){
+    // this.props.serverTest();
+    // console.log("props",this.props);
+   // call action creater which places the result into the state
+  }
+  constructor(props){
+    super(props);
+    // this.state={
+    //
+    // }
+    this.state = {
+      // this is just temporary values
+      line :null,
+      cards: [
+              {_id: "kiss", title:"title1", date:"november 4 2018"},
+              {_id: "heaven", title:"title2", date:"december 15 2018"},
+              {_id: "demons", title:"title3", date:"september 30 2018"},
+              {_id: "loveones", title:"title4", date:"september 26 2018"},
+              {_id: "pain", title:"title5", date:"september 1 2018"}
+            ]
+    };
+  }
+
+  // lineGenerator = d3.line();
+  // static getDerivedStateFromProps(props){
+  //   // this.lineGenerator(data);
+  //   console.log(data);
+  //
+  //   // return {line:this.lineGenerator};
+  //   return null;
+  // }
+  render(){
+    return (
+      <div>
+        <div className="header-box">
+          <h1 className="heading__primary heading__primary--pink u-mt-sm ">Remember</h1>
+        </div>
+
+        <div className="xcontainer-main u-mb-md">
+            <div className="topcards">
+              <h4 className="heading__quaternary u-mb-sm">Top Five Cards </h4>
+              <ul className="list">
+                 {
+                    this.state.cards.map( function(master, index){
+                      return ( <TopFiveCard key={master._id} cards={master}/>  );
+                    })
+                 }
+               </ul>
+            </div>
+
+              <Chart data={data}/>
+        </div>
+        <CreateMaster/>
+
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    test: state.test
+  };
+}
+export default connect(mapStateToProps, null)(MasterCard);
+
+// <div className="create">
+//   <h4 className="heading__quaternary u-sb-md">Create Main Cards</h4>
+//
+//
+//   <div className="create__contrainer ">
+//     <div className="create__cards  u-mb-sm">
+//
+//       <div className="create__field">
+//         <input onChange={(event) => this.setState( {title:event.target.value} )} type="text" value={this.state.title} id="title" className="input__master-title" maxLength="32" />
+//       </div>
+//
+//       <div className="create__cta u-mb-sm">
+//         <Link to={`/main/subcards/${this.state.title}`} className="create__cta-btn "> Create Master Card </Link>
+//       </div>
+//     </div>
+//   </div>
+// </div>
