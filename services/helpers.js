a3223bb6345ee114f4e1fb67b87a10ded8f2a283
NicksComingUp <remember.marsman@gmail.com>
2019-01-20 09:15:25 -0600

    add the current Date

--- services/helpers.js
+++ services/helpers.js
@@ -7,6 +7,12 @@ const helpers ={};
 helpers.hash = function(password){
   return crypto.createHmac('sha256',  keys.hashKey).update(password).digest('hex');
 }
-
+helpers.currentDate = function(current){// Date
+  // const current = new Date();
+  const year = current.getFullYear();
+  const date = current.getDate();
+  const month= current.getMonth();
+  return new Date(year, date, month);//Date
+}
 
 module.exports = helpers;
