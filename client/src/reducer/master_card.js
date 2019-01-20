ad445458c64be345d3cd1de3288d07bcc2e82fab
NicksComingUp <remember.marsman@gmail.com>
2019-01-20 09:22:09 -0600

    update the store with the master card

--- client/src/reducer/master_card.js
+++ client/src/reducer/master_card.js
@@ -1,6 +1,7 @@
 import {
   MYCARDS,
  DELETE_MASTER,
+ UPDATE_TIME,
  UPDATE_MASTER}  from '../actions/types';
 //SUBMIT_CARD,
 //  MASTER_FORM,
@@ -8,9 +9,9 @@ import {
 
 // This method is use just to test my server
 export default  function (state = [], action) {
+  // console.log(action.payload);
   switch(action.type){
     case MYCARDS:
-    // console.log(action.payload)
     return action.payload;
     case UPDATE_MASTER:
       // for(let i=0;i < state.length; i++ )
@@ -19,12 +20,9 @@ export default  function (state = [], action) {
       // UPDATED at the beginning
     return [action.payload,...state.filter(card=> card._id !== action.payload._id)];
     case DELETE_MASTER:
-      // console.log("Id of the remove card : ",action.payload)
       return state.filter(elem => elem.id !== action.payload);
-    // case TEST_CARD:
-    //   return {id:action.payload};
-    // case SUBMIT_CARD:
-    //   return {id:action.payload};
+    case  UPDATE_TIME:
+      return action.payload;
     default:
       return state;
   }
