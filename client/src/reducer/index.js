32fa3313ac2d31a13aefaf74bb91958893b65462
NicksComingUp <remember.marsman@gmail.com>
2019-01-27 18:21:24 -0600

    adding the poolcard to the store

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -4,12 +4,14 @@ import  cards  from './cardOperations';
 import  formupdate  from './updateForm';
 import  master_card  from './master_card';
 import showsubcards from './showsubcards';
+import poolcards from './poolcards';
 const rootReducer = combineReducers({
   user,
   cards,
   formupdate,
   master_card, // to test server with actionCreater
-  showsubcards
+  showsubcards,
+  poolcards
 });
 
 export default rootReducer;
