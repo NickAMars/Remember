0c78bb91509b771e4e7e0d62b3866679d7ee07b0
NicksComingUp <remember.marsman@gmail.com>
2019-01-27 18:16:15 -0600

    adding redirect for the createMasterCards

--- controllers/masterCard.js
+++ controllers/masterCard.js
@@ -4,14 +4,18 @@ const SubCard = require('../models/SubCards');
 const helper = require('../services/helpers');
 const passport        = require('passport');
 module.exports = {
-  masterCards : async (req,res) =>{
-      const findUser = await User.findById(req.user).populate('mastercards');
-      if(!findUser) res.status(500).send({});
+  masterCards : async (req,res, next) =>{
+      const findUser = await User.findById(req.user).populate(
+        {path:'mastercards',
+          populate:{
+            path: "subcards",
+            model: "subcard"
+          }
+        });
+      if(!findUser) next();//res.status(500).send({});
       res.send(findUser.mastercards);
   },
   createMasterCards : async (req,res) =>{
-    // console.log("THIS IS BEING CALLED");
-    // console.log("create one master card");
     const {title, cards } = req.body;
     const findUser = await User.findById(req.user);
     const newMaster = new MasterCard({title });
@@ -22,13 +26,10 @@ module.exports = {
         newSub.save();
     });
     newMaster.user = findUser;
-    Promise.all([findUser.save(), newMaster.save()]);
-    const allMaster = await MasterCard.find();
-    // console.log(allMaster);
-    // returning an array
-    res.send(allMaster);
+    await Promise.all([findUser.save(), newMaster.save()]);
+    res.redirect('/api/profile/MasterCard');
   },
-  updateMasterCards :async  (req,res) =>{
+  updateMasterCards :async  (req,res,next) =>{
 
     // res.status(400).send("Nothing Was Sent to update")
     if(!req.body.title) return;
@@ -36,7 +37,7 @@ module.exports = {
     // const _id = req.params.id;
     const findMaster = await MasterCard.findById(req.params.id);
     if(!findMaster)
-      return res.status(500).send("Couldnt find Master Card Information");
+      return next();//res.status(500).send("Couldnt find Master Card Information");
       findMaster.title = title;
       findMaster.timestamp = date;
       findMaster.save()
@@ -44,13 +45,11 @@ module.exports = {
     res.send(findMaster);
   },
   deleteMasterCards : async (req,res) =>{
-    // await User.remove({_id: req.params.id});
     const mastercard = await MasterCard.findById(req.params.id);
-    mastercard.remove();
-
-    // res.send("delete a master card");
+    await mastercard.remove();
+    res.send("SUCCESSFUL DELETE");
   },
-  timeMasterCards : async (req, res) => {
+  timeMasterCards : async (req, res, next) => {
     // updating information
     const findMaster = await MasterCard.findById(req.params.id);
     const length = findMaster.progress.length;
@@ -58,7 +57,7 @@ module.exports = {
     // takes the current time and look on the last time
     const daycreated = helper.currentDate(new Date()); // return date
     // {$inc: { postCount: 2}}
-    if(length === 0 ){
+    if(length === 0 ){ // if empty
       findMaster.progress.push({time, daycreated });
     }else if(findMaster.progress[length-1].daycreated.getTime() === daycreated.getTime()){
       // update the time
@@ -70,14 +69,10 @@ module.exports = {
         findMaster.progress.shift();
       }
     }
-    findMaster.save();
+    await findMaster.save();
     const findUser = await User.findById(req.user).populate('mastercards');
-    if(!findUser) res.status(500).send({});
+    if(!findUser) next();//res.status(500).send({});
     //send all the master cards back for the users
     res.send(findUser.mastercards);
-    // res.send(allMaster);
   }
 };
-
-
-// CRUD
