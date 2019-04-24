87879028ca44b288287e16f0f2231830b5410d47
NicksComingUp <remember.marsman@gmail.com>
2019-04-24 06:27:43 -0500

    adding the production keys to heroku

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports= {
+  mongoURL: process.env.mongoURL,
+  cookieKey: process.env.cookieKey,
+  hashKey: process.env.hashKey,
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  FBClientID:process.env.FBClientID,
+  FBClientSecret:process.env.FBClientSecret,
+}
