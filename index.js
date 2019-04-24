dc4b718e84c1cb9ff7c7f66b03c494d10e484aa9
NicksComingUp <remember.marsman@gmail.com>
2019-04-24 06:37:40 -0500

    adding the production keys to heroku

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ if(process.env.NODE_ENV === 'production'){
   const path = require('path');
   // if we dont recover the route, go to the index.html
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 
 }
